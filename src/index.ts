2646420317d64f3e59ca992043320b31b381d14d
ignacio-ambia <ignacio.ambia@gmail.com>
2023-08-20 21:13:00 -0600

    add dotenv package

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,17 @@
 import express, { Request, Response } from "express";
 import testRoute from "./routes/test.routes";
+import "dotenv/config";
+
+const { MONGO_USERNAME, MONGO_PASSWORD } = process.env;
 
 const app = express();
 
 const port = process.env.PORT || 3000;
 
-
-app.use('/contact', testRoute);
+app.use("/contact", testRoute);
 
 app.get("/test", (request: Request, response: Response) => {
-  response.send("Hola mundo!");
+  response.send("Hola mundo!" + MONGO_USERNAME + MONGO_PASSWORD);
 });
 
 app.listen(port, () => {
