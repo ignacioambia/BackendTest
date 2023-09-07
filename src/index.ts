52f211f46ab9e8eedd7ec7876b2477121eedd09c
ignacio-ambia <ignacio.ambia@gmail.com>
2023-09-06 21:50:56 -0600

    remove unnecessary test endpoints

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,8 @@
 import express, { Request, Response } from "express";
-import testRoute from "./routes/test.routes";
 import "dotenv/config";
 import { connectToDatabase } from "./db";
 
 const app = express();
-
-app.use("/contact", testRoute);
-
-app.get("/test", (request: Request, response: Response) => {
-  response.send("Hola mundo!");
-});
 
 connectToDatabase().then(() => {
   const port = process.env.PORT || 3000;
