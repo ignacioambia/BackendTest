28cc05a6528c053742d1ab714848bee46c70ba9c
ignacio-ambia <ignacio.ambia@gmail.com>
2023-09-25 06:45:39 -0600

    improve id exists middleware

--- src/types/express/index.d.ts
+++ src/types/express/index.d.ts
@@ -1,9 +1,11 @@
 import express from "express";
+import { Document } from 'mongoose'
 
 declare global {
   namespace Express {
     interface Request {
-     verified?: string | JwtPayload
+     verified?: string | JwtPayload,
+     element?: Document 
     }
   }
  }
